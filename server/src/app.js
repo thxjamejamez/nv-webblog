613a243df94c45a1beb3051a27b5a35fc3208639
Paisan Aunyaw <paisan.a@bda.co.th>
2021-06-09 10:55:40 +0700

    npm install

--- server/src/app.js
+++ server/src/app.js
@@ -1,29 +1,37 @@
-let express = require('express')
-let bodyParser = require('body-parser')
-let cors = require('cors')
-const {sequelize} = require('./models')
+let express = require("express");
+let bodyParser = require("body-parser");
+let cors = require("cors");
+const { sequelize } = require("./models");
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(cors())
-
-require('./userPassport')
-require('./routes')(app)
-
-app.get('/status', function (req, res) {
-  res.send('Hello nodejs server')
-})
-
-app.get('/hello/:person', function (req, res) {
-  console.log('hello - '+ req.params.person)
-  res.send('say hello with ' + req.params.person)
-})
-
-let port = 8081
-sequelize.sync({force: false}).then(() => {
-  app.listen(port, function () {
-    console.log('server running on ' + port)
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
   })
-})
+);
+app.use(cors());
+
+require("./userPassport");
+require("./routes")(app);
+
+app.get("/status", function (req, res) {
+  res.send("Hello nodejs server");
+});
+
+app.get("/hello/:person", function (req, res) {
+  console.log("hello - " + req.params.person);
+  res.send("say hello with " + req.params.person);
+});
+
+let port = 8081;
+sequelize
+  .sync({
+    force: false,
+  })
+  .then(() => {
+    app.listen(port, function () {
+      console.log("server running on " + port);
+    });
+  });
